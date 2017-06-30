179665eeef5b494197135dc48a1af27e73bf83a1
PC <pc@kvalvik.no>
2017-07-01 01:10:40 +0200

    Allow moving votes with up and down arrow keys

--- 2017/script.js
+++ 2017/script.js
@@ -45,11 +45,46 @@ function load_storage() {
     });
 }
 
+function swap_values(e1, e2) {
+    e2_value = e2.val();
+    e2.val(e1.val());
+    e1.val(e2_value);
+}
+
+function handle_swap(event) {
+    var e = $("#" + event.target.id),
+    index = parseInt(event.target.id.slice(-1)),
+    next, next_value;
+    
+    // Arrow down
+    if (event.keyCode == 40) {
+        if (index >= 9)
+            return
+        
+        next = item(index + 1);
+        swap_values(e, next);
+        next.focus();
+        update_command();
+    }
+    // Arrow up
+    else if (event.keyCode == 38) {
+        if (index <= 0)
+            return
+        
+        next = item(index - 1);
+        swap_values(e, next);
+        next.focus();
+        update_command();
+    }
+}
+
 $(document).ready(function() {
     load_storage();
+    update_command();
     
     $(".items").on("input", update_command);
     $(".items").on("focusout", save_storage);
+    $(".items").on("keyup", handle_swap);
     
     $("#copy").on("click", function() {
         $(".command").select();
