efdb883445849510c8de36cfab0367928a402c1a
PC <pc@kvalvik.no>
2017-07-01 00:25:26 +0200

    add copy to clipboard button

--- 2017/script.js
+++ 2017/script.js
@@ -1,3 +1,22 @@
+// https://stackoverflow.com/a/987376
+function select_text(element) {
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;    
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();        
+        range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
+
 function update_command() {
     var command = "!vote";
     
@@ -7,18 +26,21 @@ function update_command() {
         }
     }
     
-    $(".command").text(command);
+    $(".command").val(command);
+    $(".command").style.size = command.length;
 }
-
-/*for (i = 1; i <= 10; i++) {
-    
-}*/
 
 $(document).ready(function() {
     $(".items").each(function() {
         var e = $(this);
         e.on("input", update_command);
     });
+    
+    $("#copy").on("click", function() {
+        $(".command").select();
+        document.execCommand("copy");
+    });
 });
 
 update_command();
+
