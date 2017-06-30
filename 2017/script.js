5d14632efa9c40e11a5130d0698c731b09fbc069
PC <pc@kvalvik.no>
2017-07-01 01:16:08 +0200

    Remove redundant code

--- 2017/script.js
+++ 2017/script.js
@@ -1,22 +1,3 @@
-// https://stackoverflow.com/a/987376
-function select_text(element) {
-    var doc = document
-        , text = doc.getElementById(element)
-        , range, selection
-    ;    
-    if (doc.body.createTextRange) {
-        range = document.body.createTextRange();
-        range.moveToElementText(text);
-        range.select();
-    } else if (window.getSelection) {
-        selection = window.getSelection();        
-        range = document.createRange();
-        range.selectNodeContents(text);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-}
-
 function item(index) {
     return $("#li" + index);
 }
